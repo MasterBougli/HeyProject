77cfaa501de0a0ce8e041e7ceeffecd9ea7a2a7d
MasterBougli <nabusius@gmail.com>
2021-10-01 07:58:09 +0200

    Remove ALS

--- Plugins/Replicated-ALS/Source/ReplicatedALS/Private/Libraries/ALSEnumLibrary.cpp
+++
@@ -1,4 +0,0 @@
-// Copyright 2020-2030 AlemdarLabs, All Rights Reserved
-
-
-#include "Libraries/ALSEnumLibrary.h"
--- Plugins/Replicated-ALS/Source/ReplicatedALS/Private/Libraries/ALSStructLibrary.cpp
+++
@@ -1,3 +0,0 @@
-// Copyright 2020-2030 AlemdarLabs, All Rights Reserved
-
-#include "Libraries/ALSStructLibrary.h"
